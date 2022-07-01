e908452629c4a1e7afd7be21bdc2edbe7295a625
John Francis <jean_f_05@hotmail.com>
2022-07-01 11:05:01 +0100

    added displaycart state

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShowCart} />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food" />
